240e0b90c05897b6b79db6b1ef5e4dbe6a820bfb
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-12-04 23:46:33 +0300

    Implemented helper for UIStoryboard

---
+++ swift_welcome_home/UIStoryboard+Helper.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Evgeniy Akhmerov on 04/12/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import UIKit
+
+extension UIStoryboard {
+    static var wh_main: UIStoryboard {
+        return UIStoryboard(name: "Main", bundle: nil)
+    }
+}
